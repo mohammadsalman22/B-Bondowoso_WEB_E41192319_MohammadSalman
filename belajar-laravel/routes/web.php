001b842ac54ac5b432d8d9757068101d829751cb
mohammadsalman22 <71873673+mohammadsalman22@users.noreply.github.com>
2021-03-25 14:26:34 +0700

    Tugas 3 Pertemuan 2

--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('user', 'ManagementUserController@index');
+Route::resource('user', 'ManagementUserController');
+
+?>
